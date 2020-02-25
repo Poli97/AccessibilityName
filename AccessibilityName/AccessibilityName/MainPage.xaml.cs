71b8f21c88c2e8a3d8cf90573a8c34790679387e
Poli97 <paolo.pecis@gmail.com>
2020-02-25 11:51:25 +0100

    Accessibility name on Label visible in Android UI

---
+++ AccessibilityName/AccessibilityName/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AccessibilityName
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Label label = new Label
+            {
+                BackgroundColor = Color.Orange,
+            };
+            AutomationProperties.SetIsInAccessibleTree(label, true);
+            AutomationProperties.SetName(label, "Test accessibility name");
+            AbsoluteLayout.SetLayoutBounds(label, new Rectangle(0.5, 0.5, 0.3, 0.2));
+            AbsoluteLayout.SetLayoutFlags(label, AbsoluteLayoutFlags.All);
+
+            absolutelayout.Children.Add(label);
+        }
+    }
+}
